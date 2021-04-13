6b94eb5b11feb67a9e537e4529a76d1f2dc4b9a8
Slime-hatena <git@aki-memo.net>
2021-04-13 19:20:29 +0900

    feat: プロセスの待ち方を変更し管理者権限を不要に

--- bin/start.ps1
+++ bin/start.ps1
@@ -1,13 +1,5 @@
 ﻿Add-Type -Assembly System.Windows.Forms;
 
-# dmmgames launcherは管理者権限で動くので昇格させる
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    # steamがゲーム起動中の判定に使うためwaitしておく
-    $arg = "-ExecutionPolicy Bypass `"& '" + $myinvocation.mycommand.definition + "' ; pause;`"";
-    Start-Process powershell -Wait -Verb runAs -ArgumentList $arg;
-    exit;
-}
-
 # ゲーム起動
 Start-Process "dmmgameplayer://umamusume/cl/general/umamusume";
 $c = 0;
@@ -21,6 +13,8 @@ do {
     Get-Process -Name umamusume;
 } while (!$?);
 
-# 起動を確認したらゲーム終了まで待ち、dmmgames launcherを終了
-Wait-Process -Name umamusume;
-Stop-Process -Name DMMGamePlayer;
+do {
+    # プロセスが取得できなくなったら終了したと見なす
+    Start-Sleep -s 5;
+    Get-Process -Name umamusume;
+} while ($?);
